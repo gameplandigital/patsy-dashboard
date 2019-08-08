19adaef83fef847a16cdfed34ec2656aff471c3f
Lenny Binuya <leonardbinuya90@gmail.com>
2019-08-09 01:44:31 +0800

    WE

--- dash/air21.php
+++ dash/air21.php
@@ -21,7 +21,7 @@
   }
     
 ?>
-<?php include('pagination.php'); ?>
+<?php include('pagination-1.php'); ?>
 
 <?php
 
@@ -41,7 +41,7 @@ $date=date('F j, Y g:i:a');
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>HOME | AIRCAST | TELEMARKETING</title>
+    <title>AIRCAST | TELEMARKETING</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- favicon
--- dash/pagination-1.php
+++ dash/pagination-1.php
@@ -2,42 +2,43 @@
  
 	include("conn.php");
  
-	$query=mysqli_query($conn,"select count(id) from `lead_users_unicare`");
+	$query=mysqli_query($conn,"select count(id) from `air21_users`");
 	$row = mysqli_fetch_row($query);
  
 	$rows = $row[0];
  
-	$page_rows = 1;
+	$page_rows = 10;
  
 	$last = ceil($rows/$page_rows);
  
-	if($last < 1){
-		$last = 1;
+	if($last < 10){
+		$last = 10;
 	}
  
-	$pagenum = 1;
+	$pagenum = 10;
  
 	if(isset($_GET['pn'])){
 		$pagenum = preg_replace('#[^0-9]#', '', $_GET['pn']);
 	}
  
-	if ($pagenum < 1) { 
-		$pagenum = 1; 
+	if ($pagenum < 10) { 
+		$pagenum = 10; 
 	} 
 	else if ($pagenum > $last) { 
 		$pagenum = $last; 
 	}
  
-	$limit = 'LIMIT ' .($pagenum - 1) * $page_rows .',' .$page_rows;
+	$limit = 'LIMIT ' .($pagenum - 10) * $page_rows .',' .$page_rows;
  
-	$nquery=mysqli_query($conn,"select * from `lead_users_unicare` WHERE status = 'Callback' order by date, RAND(5) ASC $limit");
+	$nquery=mysqli_query($conn,"SELECT MessengerId,Fname,Lname,LastActive,LastClicked FROM air21_users ORDER by LastActive DESC 
+ $limit");
  
 	$paginationCtrls = '';
  
-	if($last != 1){
+	if($last != 10){
  
-	if ($pagenum > 1) {
-        $previous = $pagenum - 1;
+	if ($pagenum > 10) {
+        $previous = $pagenum - 10;
 		$paginationCtrls .= '<a href="'.$_SERVER['PHP_SELF'].'?pn='.$previous.'" class="btn btn-default">Previous</a> &nbsp; &nbsp; ';
  
 		for($i = $pagenum-4; $i < $pagenum; $i++){
@@ -49,7 +50,7 @@
  
 	$paginationCtrls .= ''.$pagenum.' &nbsp; ';
  
-	for($i = $pagenum+1; $i <= $last; $i++){
+	for($i = $pagenum+10; $i <= $last; $i++){
 		$paginationCtrls .= '<a href="'.$_SERVER['PHP_SELF'].'?pn='.$i.'" class="btn btn-default">'.$i.'</a> &nbsp; ';
 		if($i >= $pagenum+4){
 			break;
@@ -57,7 +58,7 @@
 	}
  
     if ($pagenum != $last) {
-        $next = $pagenum + 1;
+        $next = $pagenum + 10;
         $paginationCtrls .= ' &nbsp; &nbsp; <a href="'.$_SERVER['PHP_SELF'].'?pn='.$next.'" class="btn btn-default">Next</a> ';
     }
 	}
