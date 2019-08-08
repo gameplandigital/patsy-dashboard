6db87f4091dac494b536d491cc266c0e10ca79aa
Lenny Binuya <leonardbinuya90@gmail.com>
2019-08-09 01:46:12 +0800

    WE

--- dash/pagination-1.php
+++ dash/pagination-1.php
@@ -11,34 +11,34 @@
  
 	$last = ceil($rows/$page_rows);
  
-	if($last < 10){
-		$last = 10;
+	if($last < 1){
+		$last = 1;
 	}
  
-	$pagenum = 10;
+	$pagenum = 1;
  
 	if(isset($_GET['pn'])){
 		$pagenum = preg_replace('#[^0-9]#', '', $_GET['pn']);
 	}
  
-	if ($pagenum < 10) { 
-		$pagenum = 10; 
+	if ($pagenum < 1) { 
+		$pagenum = 1; 
 	} 
 	else if ($pagenum > $last) { 
 		$pagenum = $last; 
 	}
  
-	$limit = 'LIMIT ' .($pagenum - 10) * $page_rows .',' .$page_rows;
+	$limit = 'LIMIT ' .($pagenum - 1) * $page_rows .',' .$page_rows;
  
 	$nquery=mysqli_query($conn,"SELECT MessengerId,Fname,Lname,LastActive,LastClicked FROM air21_users ORDER by LastActive DESC 
- $limit");
+ desc $limit");
  
 	$paginationCtrls = '';
  
-	if($last != 10){
+	if($last != 1){
  
-	if ($pagenum > 10) {
-        $previous = $pagenum - 10;
+	if ($pagenum > 1) {
+        $previous = $pagenum - 1;
 		$paginationCtrls .= '<a href="'.$_SERVER['PHP_SELF'].'?pn='.$previous.'" class="btn btn-default">Previous</a> &nbsp; &nbsp; ';
  
 		for($i = $pagenum-4; $i < $pagenum; $i++){
@@ -50,7 +50,7 @@
  
 	$paginationCtrls .= ''.$pagenum.' &nbsp; ';
  
-	for($i = $pagenum+10; $i <= $last; $i++){
+	for($i = $pagenum+1; $i <= $last; $i++){
 		$paginationCtrls .= '<a href="'.$_SERVER['PHP_SELF'].'?pn='.$i.'" class="btn btn-default">'.$i.'</a> &nbsp; ';
 		if($i >= $pagenum+4){
 			break;
@@ -58,7 +58,7 @@
 	}
  
     if ($pagenum != $last) {
-        $next = $pagenum + 10;
+        $next = $pagenum + 1;
         $paginationCtrls .= ' &nbsp; &nbsp; <a href="'.$_SERVER['PHP_SELF'].'?pn='.$next.'" class="btn btn-default">Next</a> ';
     }
 	}
