7c2613c1f1cbbc1b81919cfeee3dd402ea1a4ca3
Lenny Binuya <leonardbinuya90@gmail.com>
2019-08-14 17:18:02 +0800

    WE

---
+++ dash/db_connect.php
@@ -0,0 +1,7 @@
+<?php
+$servername = "patsydb.com4k2xtorpw.ap-southeast-1.rds.amazonaws.com";
+$username = "patsydigital01";
+$password = "pAtsy06072018";
+$dbname = "patsy_db";
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+?>
--- dash/rfc.php
+++ dash/rfc.php
@@ -112,6 +112,39 @@ $date=date('F j, Y g:i:a');
 <!-- END Monitoring -->
 
 
+<!-- PAGINATION - BETA -->
+
+
+
+<?php
+include_once("db_connect.php");
+$showRecordPerPage = 5;
+if(isset($_GET['page']) && !empty($_GET['page'])){
+$currentPage = $_GET['page'];
+}else{
+$currentPage = 1;
+}
+$startFrom = ($currentPage * $showRecordPerPage) - $showRecordPerPage;
+$totalEmpSQL = "SELECT * FROM rfc_apply";
+$allEmpResult = mysqli_query($conn, $totalEmpSQL);
+$totalEmployee = mysqli_num_rows($allEmpResult);
+$lastPage = ceil($totalEmployee/$showRecordPerPage);
+$firstPage = 1;
+$nextPage = $currentPage + 1;
+$previousPage = $currentPage - 1;
+$empSQL = "SELECT *
+FROM `rfc_apply` LIMIT $startFrom, $showRecordPerPage";
+$empResult = mysqli_query($conn, $empSQL);
+?> 
+
+
+
+
+
+
+<!-- --> 
+
+
 
 <!doctype html>
 <html class="no-js" lang="ca">
@@ -332,35 +365,50 @@ $date=date('F j, Y g:i:a');
                                 </div>
                                 <div id="menu1" class="tab-pane fade">
                                     <div class="tab-ctn">
-                                         <table cellpadding="0" cellspacing="0" border="0" class="table table-striped table-bordered" id="example">
-                        <thead>
-                            <tr>
-                                <th>MessengerID</th>
-                                <th>First</th>
-                                <th>Middle</th>
-                                <th>Last</th>
-                                <th>Marital</th>
-                                <th>Gender</th>
-                                <th>View Info</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php
-                            while($crow = mysqli_fetch_array($nquery)){
-                            ?>
-                            <tr class="odd gradeX">
-                                <td><?php echo $crow['MessengerId']; ?></td>
-                                <td><?php echo $crow['Profile_pic']; ?></td>
-                                <td><?php echo $crow['Fname']; ?></td>
-                                <td><?php echo $crow['Lname']; ?></td>
-                                <td><?php echo $crow['LastActive']; ?></td>
-                                <td><?php echo $crow['LastClicked']; ?></td>
-                                
-                                <!--  -->
-                            </tr>
-                            <?php } ?>
-                        </tbody>
-                    </table>
+                                      <table class="table " cellpadding="0" cellspacing="0" border="0" class="table table-striped table-bordered" id="example">
+                                <thead>
+                                <tr>
+                                <th>EmpID</th>
+                                <th>Name</th>
+                                <th>Salary</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <?php
+                                while($emp = mysqli_fetch_assoc($empResult)){
+                                ?>
+                                <tr>
+                                <th scope="row"><?php echo $emp['id']; ?></th>
+                                <td><?php echo $emp['employee_name']; ?></td>
+                                <td><?php echo $emp['employee_salary']; ?></td>
+                                </tr>
+                                <?php } ?>
+                                </tbody>
+                                </table>
+
+                                <nav aria-label="Page navigation">
+                                    <ul class="pagination">
+                                    <?php if($currentPage != $firstPage) { ?>
+                                    <li class="page-item">
+                                    <a class="page-link" href="?page=<?php echo $firstPage ?>" tabindex="-1" aria-label="Previous">
+                                    <span aria-hidden="true">First</span>
+                                    </a>
+                                    </li>
+                                    <?php } ?>
+                                    <?php if($currentPage >= 2) { ?>
+                                    <li class="page-item"><a class="page-link" href="?page=<?php echo $previousPage ?>"><?php echo $previousPage ?></a></li>
+                                    <?php } ?>
+                                    <li class="page-item active"><a class="page-link" href="?page=<?php echo $currentPage ?>"><?php echo $currentPage ?></a></li>
+                                    <?php if($currentPage != $lastPage) { ?>
+                                    <li class="page-item"><a class="page-link" href="?page=<?php echo $nextPage ?>"><?php echo $nextPage ?></a></li>
+                                    <li class="page-item">
+                                    <a class="page-link" href="?page=<?php echo $lastPage ?>" aria-label="Next">
+                                    <span aria-hidden="true">Last</span>
+                                    </a>
+                                    </li>
+                                    <?php } ?>
+                                    </ul>
+                                    </nav>
 
 
                                     </div>
