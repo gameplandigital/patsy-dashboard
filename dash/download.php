b1e4de002e861555fb87b1b0daac273ec11951a7
joe <joelibramonte123@gmail.com>
2019-08-23 16:55:32 +0800

    1

--- dash/download.php
+++ dash/download.php
@@ -2,13 +2,21 @@
 	$file = './download/'.$_GET['file'];
    	$title=$_GET['file'];
 
-    header("Pragma: public");
-    header('Content-disposition: attachment; filename='.$title);
+    // header("Pragma: public");
+    // header('Content-disposition: attachment; filename='.$title);
   
+
+	header('Content-Description: File Transfer');
+	header('Content-Type: application/octet-stream');
+	header('Content-Disposition: attachment; filename='.basename($file));
+	header('Expires: 0');
+	header('Cache-Control: must-revalidate');
+	header('Pragma: public');
+	header('Content-Length: ' . filesize($file));
 
 	
     
-    header('Content-Transfer-Encoding: binary');
+    // header('Content-Transfer-Encoding: binary');
     ob_clean();
     flush();
 
