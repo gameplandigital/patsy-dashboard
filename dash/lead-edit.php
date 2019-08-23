426b55ef1d3ebdbb7bba320500bceb8f806da666
joe <joelibramonte123@gmail.com>
2019-08-23 11:07:22 +0800

    1

--- dash/lead-edit.php
+++ dash/lead-edit.php
@@ -232,7 +232,7 @@ $(document).ready(function(){
                                 <td><?php echo $row['doc1']; ?></td>
                                 <td><?php echo $row['doc2']; ?></td>
                                 <td><?php echo $row['doc3']; ?></td>
-                                <td><?php echo $row['doc4']; ?></td>
+                                <td><a href=<?php echo $row['doc4']; ?>></a></td>
                                 <td><?php echo $row['register_date']; ?></td>                            </tr>
                             <?php } ?>
                         </tbody>
