bc22fa3e209fc98452c15f89c44034ce63d3d114
joe <joelibramonte123@gmail.com>
2019-08-23 15:19:18 +0800

    1

--- dash/download.php
+++ dash/download.php
@@ -19,7 +19,6 @@ if(isset($_GET['doc2']))
 			header('Content-Description: File Transfer');
 			header('Content-Type: application/octet-stream');
 			header('Content-Disposition: attachment; filename='.basename($file));
-			header('Content-Transfer-Encoding: binary');
 			header('Expires: 0');
 			header('Cache-Control: must-revalidate');
 			header('Pragma: public');
