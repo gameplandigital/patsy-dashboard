9ee47c1da4ac8a68ac9891fa1073050fb8d7893b
joe <joelibramonte123@gmail.com>
2019-08-23 10:30:36 +0800

    1

--- dash/lead-edit.php
+++ dash/lead-edit.php
@@ -78,6 +78,30 @@ $(document).ready(function(){
         ============================================ -->
     <script src="js/vendor/modernizr-2.8.3.min.js"></script>
 </head>
+<!-- HEADER -->
+
+<div class="header-top-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                    <div class="logo-area">
+                        <a href="#"><img src="img/logo/logo.png" alt="" /></a>
+                    </div>
+                </div>
+                <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
+                    <div class="header-top-menu">
+                        <ul class="nav navbar-nav notika-top-nav">
+                           
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+<!-- END HEADER -->
+
 <body>
 <div class="icon-bar">
   <a href="home.php"><i class="fa fa-home"></i></a> 
