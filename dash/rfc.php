db0cef2d2e0d0e5fdcfdec20e94a69f60f695e27
joe <joelibramonte123@gmail.com>
2019-09-10 10:34:11 +0800

    1

--- dash/rfc.php
+++ dash/rfc.php
@@ -232,57 +232,50 @@ $empResult = mysqli_query($conn, $empSQL);
 
     <br>
 
-       <!-- Start Status area -->
+    <!-- Start Status area -->
     <div class="notika-status-area">
-        <center><div class="container">
+        <div class="container">
             <div class="row">
-                <div class="col-md-1 col-md-7 col-sm-6 col-xs-12">
-                        <div class="wb-traffic-inner notika-shadow sm-res-mg-t-30 tb-res-mg-t-30">
-                            <div class="website-traffic-ctn">
-                                <h2><span></span></h2>
-                                <p></p>
-                            </div>
-                        </div>
-                    </div>
-
-                <div class="col-lg-3 col-md-8 col-sm-6 col-xs-12">
-                    <div class="wb-traffic-inner notika-shadow sm-res-mg-t-30 tb-res-mg-t-30">
-                        <div class="website-traffic-ctn">
-                            <h2><span class="counter"><?php echo $Total_Applicant; ?></span></h2>
-                            <p>Loan Applicants</p>
-                        </div>
-                    </div>
-                </div>
-                 <div class="col-lg-3 col-md-8 col-sm-6 col-xs-12">
+                <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
                     <div class="wb-traffic-inner notika-shadow sm-res-mg-t-30 tb-res-mg-t-30">
                         <div class="website-traffic-ctn">
                             <h2><span class="counter"><?php echo $Total_InCorrect; ?></span></h2>
-                            <p>Phase two Subscriber</p>
+                            <p>Total Bot User (Phase 2)</p>
                         </div>
+                        <div class="sparkline-bar-stats1">9,4,8,6,5,6,4,8,3,5,9,5</div>
                     </div>
                 </div>
-                 <div class="col-lg-3 col-md-8 col-sm-6 col-xs-12">
+                <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+                    <div class="wb-traffic-inner notika-shadow sm-res-mg-t-30 tb-res-mg-t-30">
+                        <div class="website-traffic-ctn">
+                            <h2><span class="counter"><?php echo $Total_Applicant; ?></span></h2>
+                            <p>Total Loan Applicants</p>
+                        </div>
+                        <div class="sparkline-bar-stats2">1,4,8,3,5,6,4,8,3,3,9,5</div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
                     <div class="wb-traffic-inner notika-shadow sm-res-mg-t-30 tb-res-mg-t-30 dk-res-mg-t-30">
                         <div class="website-traffic-ctn">
                             <h2><span class="counter"><?php echo $Total_New; ?></span></h2>
-                            <p>Total Bot Subscriber</p>
+                            <p>Total Bot Subscriber (Phase 1)</p>
                         </div>
+                        <div class="sparkline-bar-stats3">4,2,8,2,5,6,3,8,3,5,9,5</div>
                     </div>
                 </div>
-
-                <div class="col-md-1 col-md-7 col-sm-6 col-xs-12">
-                        <div class="wb-traffic-inner notika-shadow sm-res-mg-t-30 tb-res-mg-t-30">
-                            <div class="website-traffic-ctn">
-                                <h2><span></span></h2>
-                                <p></p>
-                            </div>
+                <!-- <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+                    <div class="wb-traffic-inner notika-shadow sm-res-mg-t-30 tb-res-mg-t-30 dk-res-mg-t-30">
+                        <div class="website-traffic-ctn">
+                            <h2><span class="counter">1,000</span></h2>
+                            <p>Total Support Tickets</p>
                         </div>
-                </div>
+                        <div class="sparkline-bar-stats4">2,4,8,4,5,7,4,7,3,5,7,5</div>
+                    </div>
+                </div> -->
             </div>
-        </div></center>
+        </div>
     </div>
     <!-- End Status area-->
-
 
 
 
