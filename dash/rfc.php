72d22032edd42823f68c38985842982ec9a9312c
joe <joelibramonte123@gmail.com>
2019-09-10 09:59:34 +0800

    1

--- dash/rfc.php
+++ dash/rfc.php
@@ -148,22 +148,10 @@ $empResult = mysqli_query($conn, $empSQL);
 
 <!doctype html>
 <html class="no-js" lang="ca">
-<style>
-#Row1 {
-  background: #c7c9d3;
-  color:#fff;
-  height:220px;
-  margin-top:15px;
-}
 
-#Row2 {
-  height:350px;
-  margin-top:15px;
-}
-.card{
-  border: 1px solid rgba(0, 0, 0, 0)!important;
-}
-</style>
+    <style>
+
+    </style>
 
 <head>
     <meta charset="utf-8">
@@ -246,39 +234,94 @@ $empResult = mysqli_query($conn, $empSQL);
     </div>
     <!-- End Header Top Area -->
 
-    <br>
+          <!-- Page Heading -->
+          <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+            <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
+          </div>
 
-    <div class="container-fluid"> 
-  <div class="row">
-    <div class="col-sm-4">
-      <div class="card">
-        <div class="card-body" id="Row1">
-          <h5 class="card-title">Chart Data</h5>
-          <p class="card-text" style="font-size:6em;text-align:center;margin-top:-20px">23%</p>
-        </div>
-      </div>
-    </div>
-        <div class="col-sm-4">
-      <div class="card">
-        <div class="card-body" id="Row1">
-          <h5 class="card-title">Status Data</h5>
-          <p class="card-text" style="font-size:6em;text-align:center;margin-top:-20px">-4.9%<i class="fa fa-arrow-down" aria-hidden="true"></i></p>
-        </div>
-      </div>
-    </div>
-    <div class="col-sm-4">
-      <div class="card">
-        <div class="card-body" id="Row1">
-          <h5 class="card-title">New Data</h5>
-          <p class="card-text" style="font-size:6em;text-align:center;margin-top:-20px">54.3%</p>
-        </div>
-      </div>
-    </div>
+          <!-- Content Row -->
+          <div class="row">
 
+            <!-- Earnings (Monthly) Card Example -->
+            <div class="col-xl-3 col-md-6 mb-4">
+              <div class="card border-left-primary shadow h-100 py-2">
+                <div class="card-body">
+                  <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                      <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Earnings (Monthly)</div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
+                    </div>
+                    <div class="col-auto">
+                      <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
 
+            <!-- Earnings (Monthly) Card Example -->
+            <div class="col-xl-3 col-md-6 mb-4">
+              <div class="card border-left-success shadow h-100 py-2">
+                <div class="card-body">
+                  <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                      <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Earnings (Annual)</div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
+                    </div>
+                    <div class="col-auto">
+                      <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
 
+            <!-- Earnings (Monthly) Card Example -->
+            <div class="col-xl-3 col-md-6 mb-4">
+              <div class="card border-left-info shadow h-100 py-2">
+                <div class="card-body">
+                  <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                      <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Tasks</div>
+                      <div class="row no-gutters align-items-center">
+                        <div class="col-auto">
+                          <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">50%</div>
+                        </div>
+                        <div class="col">
+                          <div class="progress progress-sm mr-2">
+                            <div class="progress-bar bg-info" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-auto">
+                      <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
 
-    <br>
+            <!-- Pending Requests Card Example -->
+            <div class="col-xl-3 col-md-6 mb-4">
+              <div class="card border-left-warning shadow h-100 py-2">
+                <div class="card-body">
+                  <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                      <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Pending Requests</div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
+                    </div>
+                    <div class="col-auto">
+                      <i class="fas fa-comments fa-2x text-gray-300"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <!-- Content Row -->
 
  
     <!-- Breadcomb area Start-->
